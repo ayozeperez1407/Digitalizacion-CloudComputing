0d1b436755c2d2c308281691859d806fe261c96e
YOYOPULULU <ayozeperez1407@gmail.com>
2026-04-23 23:50:29 +0200

    Update CloudServer.java

--- CloudServer.java
+++ CloudServer.java
@@ -4,17 +4,26 @@ import java.util.List;
 public class CloudServer {
 
     private final List<SensorData> dataList = new ArrayList<>();
+    private int totalAlerts = 0;
 
     public void saveData(SensorData data){
         dataList.add(data);
-        System.out.println("[CLOUD] Dato guardado "+ data);
+        System.out.println("[CLOUD] Dato guardado " + data);
+    }
+
+    public void incrementAlerts() {
+        totalAlerts++;
+    }
+
+    public int getTotalAlerts() {
+        return totalAlerts;
     }
 
     public void showData(){
         double sum = 0;
-        double average = sum /dataList.size();
 
         System.out.println("\n[CLOUD] Historial de datos:");
+        
         if(dataList.isEmpty()){
             System.out.println("No hay datos guardados");
             return;
@@ -24,12 +33,11 @@ public class CloudServer {
             System.out.println(data);
             sum += data.getTEMPERATURE();
         }
-        
-        
-        System.out.println("Media de temperatura: "+ String.format("%.2f", average));
-        
 
+        double average = sum / dataList.size();
+
+        System.out.println("Total datos: " + dataList.size());
+        System.out.println("Media de temperatura: " + String.format("%.2f", average));
+        System.out.println("Total alertas globales: " + totalAlerts);
     }
-
-
 }
