4e9c1077017bba6f1460ddb23949247418d991a5
YOYOPULULU <ayozeperez1407@gmail.com>
2026-04-23 23:51:17 +0200

    Update EdgeNode.java

--- EdgeNode.java
+++ EdgeNode.java
@@ -1,7 +1,7 @@
 public class EdgeNode {
+
     private final FogNode fogNode;
     private int id;
-
 
     public EdgeNode(FogNode fogNode, int id){
         this.fogNode = fogNode;
@@ -13,18 +13,30 @@ public class EdgeNode {
     }
 
     public void sendData(int totalReadings){
-    for (int i = 1; i <= totalReadings; i++){
-        if (fogNode.getAlertCount() < 20){
-            double temperature = 20 + Math.random() * 15 + 1;
-            SensorData data = new SensorData("sensor", temperature);
-            // Nuevo mensaje con Fog y Edge
-            System.out.println("Fog node " + fogNode.getId() + " Edge node " + this.id + ": temperatura " + String.format("%.2f", temperature));
+
+        for (int i = 1; i <= totalReadings; i++){
+
+            // parar sistema global en 20 alertas
+            if (fogNode.getCloudServer().getTotalAlerts() >= 20){
+                System.out.println("⛔ Límite global de alertas alcanzado");
+                break;
+            }
+
+            double temperature = 20 + Math.random() * 16;
+
+            SensorData data = new SensorData(
+                "sensor-F" + fogNode.getId() + "-E" + id,
+                temperature
+            );
+
+            System.out.println("Fog " + fogNode.getId() +
+                    " Edge " + id +
+                    " → temperatura: " +
+                    String.format("%.2f", temperature));
+
             fogNode.processData(data);
+
             System.out.println();
-        } else {
-            break;
         }
     }
-}
-
 }
